2a81bfff7a986b06c1dc4063bc4491134e7f4733
Alexis Aguilar <alexis.aguilar1892@gmail.com>
2024-09-16 17:14:52 -0500

    setup files with guidelines

---
+++ rec04/rec04A.sh
@@ -0,0 +1,15 @@
+# Write a bash script that accepts a person’s name as a command-line argument and
+# simply prints out the following greeting: "Good day, <name_entered>! Nice to meet
+# you!", where <name_entered> is a positional parameter (i.e., a command-line
+# argument) passed to the script. If the user does not enter a command-line argument
+# when invoking this shell script, you will simply output: "Hope you have a great day!",
+# without any name displayed. This bash script should be called rec04A.sh.
+# 
+# SAMPLE OUTPUT (input in bold):
+# $ ./rec04A.sh Mark
+# Good day, Mark! Nice to meet you!
+# $ ./rec04A.sh
+# Hope you have a great day!
+
+#!/usr/bin/bash
+
---
+++ rec04/rec04B.sh
@@ -0,0 +1,22 @@
+# Write a bash script that (1) prompts the user to enter a Linux command and reads in
+# the user’s input, (2) prints out a meaningful message with the user’s entered
+# command to be executed, and finally (3) executes the user’s entered command. The
+# Linux command is to be read in, not passed as a command-line argument. This bash
+# script should be called rec04B.sh.
+#
+# SAMPLE OUTPUT (input in asterisks):
+# $ ./rec04B.sh
+# Enter Linux command to perform: **ls**
+# Command to be executed: ls
+# a.out Lab01A.c Lab02B.sh minor1.c minor2.c
+# $ ./rec04B.sh
+# Enter Linux command to perform: **ls -l**
+# Command to be executed: ls -l
+# total 44
+# -rwx------ 1 mat0299 mat0299 7668 Feb 2 15:57 a.out
+# -rw------- 1 mat0299 mat0299 66 Feb 9 00:04 Lab01A.c
+# -rwx------ 1 mat0299 mat0299 281 Feb 10 2019 Lab02B.sh
+# -rw------- 1 mat0299 mat0299 2055 Oct 4 09:38 minor1.c
+# -rw------- 1 mat0299 mat0299 3145 Oct 8 19:47 minor2.c
+
+#!/usr/bin/bash
