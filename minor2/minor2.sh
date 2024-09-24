600907f1870052363745285932ada32b6882520c
Aguilar, Alex <alexaguilar2@my.unt.edu>
2024-09-23 19:11:49 -0500

    complete bash script for minor2.sh

--- minor2/minor2.sh
+++ minor2/minor2.sh
@@ -1,17 +1,18 @@
 # SCRIPT DESCRIPTION:
 # Write a complete bash script that monitors who logs in and logs out of the current CSE
 # Linux machine (e.g., cell01, cell02, etc.) where the script is running.
-# - You will check the status of who is logged in every 10 seconds and report each
-# user ID that logs in or logs out during that time frame. Note that there may be
-# more than one user that logs in or out during this interval.
-# - You must include the host name when reporting the user ID information.
-# - In addition, you will install a custom signal handler to trap the SIGINT (i.e., ^C)
-# one time before you are able to terminate the script with the SIGINT.
-# - You will also print out the current date and time and report the number of users
-# logged in, even if there are no changes (i.e., no one logged in or out during the
-# interval).
+#   - You will check the status of who is logged in every 10 seconds and report each
+#   user ID that logs in or logs out during that time frame. Note that there may be
+#   more than one user that logs in or out during this interval.
+#   - You must include the host name when reporting the user ID information.
+#   - In addition, you will install a custom signal handler to trap the SIGINT (i.e., ^C)
+#   one time before you are able to terminate the script with the SIGINT.
+#   - You will also print out the current date and time and report the number of users
+#   logged in, even if there are no changes (i.e., no one logged in or out during the
+#   interval).
 # Please see the SAMPLE OUTPUT for several examples, including the different
 # scenarios that might occur.
+
 # REQUIREMENTS:
 # • Your script should be well documented in terms of comments. For example, good
 # comments in general consist of a header (with your name, course section, date,
@@ -53,3 +54,62 @@
 # Thu Jun 13 01:06:45 CST 2024) # of users: 9
 # ^C (SIGINT) ignored. enter ^C 1 more time to terminate program.
 # Thu Jun 13 01:06:52 CST 2024) # of users: 9 ^C
+
+#!/bin/bash
+
+# minor2.sh
+# Script to monitor user login/logout activities on the current CSE Linux machine
+# It checks every 10 seconds and reports changes
+# Includes custom signal handler for SIGINT
+
+# Function to handle SIGINT (Ctrl+C)
+sigint_handler() {
+  if [[ $SIGINT_COUNT -lt 1 ]]; then
+    echo "^C (SIGINT) ignored. enter ^C 1 more time to terminate program."
+    SIGINT_COUNT=$((SIGINT_COUNT+1))
+  else
+    echo "Terminating the script..."
+    exit 0
+  fi
+}
+
+# Register the SIGINT handler
+trap sigint_handler SIGINT
+
+# Variables
+HOSTNAME=$(hostname)  # Get the hostname of the machine
+PREVIOUS_USERS=$(who | awk '{print $1}')  # Get the list of currently logged-in users
+SIGINT_COUNT=0  # Track the number of SIGINT occurrences
+
+# Main loop
+while true; do
+  # Capture the current date and time
+  CURRENT_TIME=$(date "+%a %b %d %T %Z %Y")
+  
+  # Get the list of currently logged-in users
+  CURRENT_USERS=$(who | awk '{print $1}')
+  
+  # Compare previous and current user lists
+  # Report if anyone has logged in or out
+  for user in $CURRENT_USERS; do
+    if [[ ! " $PREVIOUS_USERS " =~ " $user " ]]; then
+      echo "> $user logged in to $HOSTNAME"
+    fi
+  done
+
+  for user in $PREVIOUS_USERS; do
+    if [[ ! " $CURRENT_USERS " =~ " $user " ]]; then
+      echo "> $user logged out of $HOSTNAME"
+    fi
+  done
+
+  # Display the current time and the number of logged-in users
+  NUM_USERS=$(echo "$CURRENT_USERS" | wc -l)
+  echo "$CURRENT_TIME) # of users: $NUM_USERS"
+  
+  # Update the previous user list
+  PREVIOUS_USERS=$CURRENT_USERS
+
+  # Wait for 10 seconds before the next check
+  sleep 10
+done
